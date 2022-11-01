8a76f6b22c2da856a2a21c876e0de1bed6763898
Marcos Silveira <marcosilveira.lg@gmail.com>
2022-11-01 11:47:01 -0300

    Finalizado

--- game/main.py
+++ game/main.py
@@ -1,18 +1,44 @@
-from controller import atualizar, estatisticas, sorteioPalavras
+from controller import atualizar, estatisticas, sorteioPalavras, jogo
+from os import system
+
 
 def menu():
-    print('JOGO DA FORÇA'.center(25, ' '))
-    print('Escolha uma das opções:')
-    opcao = int(input('1 - Jogar \n2 - Estatísticas \n3 - Sair\n=> '))
+    """
+    Função principal contendo o menu do jogo
+    """
     
-    if opcao == 1:  # JOGAR
-        print()
-        palavra = sorteioPalavras().strip()     # função que escolhe a palavra referente ao nível selecionado
+    while True:                     # Loop do jogo
+        system('cls')               # "Limpa" a tela
+        
+        print('JOGO DA FORÇA'.center(25, ' '))      # Topo
+        
+        # MENU
+        print('Escolha uma das opções:')
+        opcao = int(input('1 - Jogar \n2 - Estatísticas \n3 - Sair\n=> '))
+        
+        if opcao == 1:  # JOGAR
+            nome = str(input('Insira seu nome: ')).strip().title()      # Inserção do nome do player
+            print()     # Print para quebra de linha
+            
+            palavra = sorteioPalavras()     # Função que escolhe a palavra referente ao nível selecionado
+            resultado = jogo(palavra)       # Resultado do jogo (True/False) será atribuido a variável resultado
+            
+            atualizar(nome, resultado)      # Função que atualiza o histórico do jogador
+            
+            enter = input('Precione ENTER para sair ')      # Enter para trocar de "tela"
+
+
+        elif opcao == 2:    # ESTATISTICAS
+            estatisticas()          # Função que mostra as estatisticas do jogo e jogadores
+            enter = input('Precione ENTER para sair ')      # Enter para trocar de "tela"
         
         
-    print(palavra)
-    print(palavra[-1])
-    
-    
-    
+        elif opcao == 3:    # SAIR
+            break
+        
+        
+        else:       # OPÇÃO INCORRETA
+            print('Opção incorreta!')
+
+
 menu()
